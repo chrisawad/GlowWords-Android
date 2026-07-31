a7922e2956f667871cb1f604bdbe62598ee4d462
Chris Awad <chrisawad@users.noreply.github.com>
2026-07-30 20:37:10 -0400

    Use stable Android API 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.webviewtest"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.webviewtest"
         minSdk = 23
-        targetSdk = 37
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
     }
